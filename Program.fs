31c4e1d978f6f296c97b8f0c21801e5a743be13f
Fareez Ahamed <fareez.ahamed@gmail.com>
2019-08-26 21:54:41 +0530

    Updated

--- Program.fs
+++ Program.fs
@@ -2,7 +2,7 @@
 open System
 open Obj
 
-type Overs = int * int
+type Overs = uint8 * uint8
 
 type Runs = 
     | Runs of int
@@ -48,63 +48,49 @@ type BowlerDetail = {
     Maidens: int
 }
 
-type BattingTeam = TeamA | TeamB | NotSet
-
-type Innings = {
-    Balls: list<Ball>
-    Runs: int
-    Wickets: int
-    Batting: list<BatsmanDetail>
-    Bowling: list<BowlerDetail>
-    BattingTeam: BattingTeam
-}
-
-type MatchState = NotStarted | FirstInnings | SecondInnings | Completed
-
 type Team = {
     Name: string
     Players: list<Player>
 }
 
-type Match = {
-    TeamSize: int
-    Overs: int
-    TeamA: Team
-    TeamB: Team
-    FirstInnings: Innings
-    SecondInnings: Innings
-    State: MatchState
+type Innings = {
+    Balls: Ball
+    BattingTeam: Team
+    BowlingTeam: Team
+    Score: uint16
+    Wickets: uint8
+    Over: Overs
+    FaceBatsman: BatsmanDetail
+    OtherBatsman: BatsmanDetail
+    Bowler: BowlerDetail
+    BowlersList: Player list
+    BatsmenList: Player list
 }
 
-let mutable m : Match = {
-    TeamSize = 11
-    Overs = 0
-    TeamA = {
-        Name = ""
-        Players = []
-    }
-    TeamB = {
-        Name = ""
-        Players = []
-    }
-    FirstInnings = {
-        BattingTeam = NotSet
-        Balls = []
-        Runs = 0
-        Wickets = 0
-        Batting = []
-        Bowling = []
-    }
-    SecondInnings = {
-        BattingTeam = NotSet
-        Balls = []
-        Runs = 0
-        Wickets = 0
-        Batting = []
-        Bowling = []
-    }
-    State = NotStarted
+type MatchBeforeStart = {
+    Teams: ( Team * Team )
+    Overs: uint8
 }
+
+type MatchOnFirstInnings = Innings
+
+type MatchOnSecondInnings = ( Innings * Innings )
+
+type MatchResult = 
+    | WonByRuns of (Team * uint16)
+    | WonByWickets of (Team * uint8)
+    | Draw
+
+type MatchCompleted = ( Innings * Innings * MatchResult)
+
+type Match = 
+    | BeforeStart of MatchBeforeStart 
+    | FirstInnings of MatchOnFirstInnings 
+    | SecondInnings of MatchOnSecondInnings
+    | Completed of MatchCompleted
+    | None
+
+let mutable m : Match = None
 
 type ScoreCommand = 
     | Runs of int 
@@ -119,18 +105,6 @@ type Command =
     | ScoreCommand of ScoreCommand 
     | PrintScore
     | Test
-
-// let incrementScore n =
-//     thisMatch.State
-//     |> function
-//         | NotStarted -> ()
-//         | FirstInnings -> 
-//             thisMatch.FirstInnings <- 
-//                 { thisMatch.FirstInnings with Runs = thisMatch.FirstInnings.Runs + n }
-//         | SecondInnings -> 
-//             thisMatch.SecondInnings <- 
-//                 { thisMatch.SecondInnings with Runs = thisMatch.SecondInnings.Runs + n }
-//         | Completed -> ()
 
 let getRuns (cmd: ScoreCommand) =
     match cmd with
@@ -157,11 +131,11 @@ let getOver (over, ball) (prevBall) (cmd: ScoreCommand) =
     | (6, _) -> (over + 1, 0)
     | (_, _) -> (over, ball + 1)
 
-let updateInnings (innings: Innings) (ball: ScoreCommand) : Innings =
-    match ball with
-    | Runs(n) -> { innings with Runs = innings.Runs + n }
-    | Wide(n) -> { innings with Runs = innings.Runs + n }
-    | NoBall(n) -> { innings with Runs = innings.Runs + n }
+// let updateInnings (innings: Innings) (ball: ScoreCommand) : Innings =
+//     match ball with
+//     | Runs(n) -> { innings with Runs = innings.Runs + n }
+//     | Wide(n) -> { innings with Runs = innings.Runs + n }
+//     | NoBall(n) -> { innings with Runs = innings.Runs + n }
 
 let parseCommand (cmd: string) = 
     let tokens = cmd.Split(" ")
@@ -169,11 +143,11 @@ let parseCommand (cmd: string) =
     | [| "exit" |] -> Exit
     | [| "help" |] -> Help
     | [| "score" |] -> PrintScore
-    | [| "r" ; n |] -> ScoreCommand( Runs <| int n )
-    | [| "nb" |] -> ScoreCommand( NoBall 0 )
-    | [| "nb" ; n |] -> ScoreCommand( NoBall <| int n )
-    | [| "wd" |] -> ScoreCommand( Wide 0 )
-    | [| "wd" ; n |] -> ScoreCommand( Wide <| int n )
+    | [| "r" ; n |] -> int n |> Runs |> ScoreCommand
+    | [| "nb" |] -> NoBall 0 |> ScoreCommand
+    | [| "nb" ; n |] -> int n |> NoBall |> ScoreCommand
+    | [| "wd" |] -> Wide 0 |> ScoreCommand
+    | [| "wd" ; n |] -> int n |> Wide |> ScoreCommand
     | [| "init" |] -> InitializeMatch
     | [| "test" |] -> Test
     | _ -> InvalidCommand
@@ -208,48 +182,21 @@ let readOptions label (options: list<'a>) : 'a =
                     | None -> printfn "Invalid Option"
                               selectOption()
     options |> List.mapi optionStr |> List.iter (printfn "%s")
-    printf "%s : " label
+    printfn "%s : " label
     selectOption()
-
-let readTossOutcome() = 
-    readOptions "Who is batting first?" [ TeamA; TeamB ]
 
 let newBatsmanDetail (player: Player) (status: BatsmanStatus) : BatsmanDetail =
         { Batsman = player; BallsFaced = 0; Runs = 0; Status = status }
 
-let readPlayer label team : Player =
-    match team with
-    | TeamA -> readOptions label m.TeamA.Players
-    | TeamB -> readOptions label m.TeamB.Players
-    | NotSet -> failwith("Team not set")
-    
-let readBatsman label =
-    match m.State with
-    | NotStarted -> failwith "Match not started"
-    | FirstInnings -> readPlayer label m.FirstInnings.BattingTeam
-    | SecondInnings -> readPlayer label m.SecondInnings.BattingTeam
-    | Completed -> failwith "Match completed"
-
-let readBowler label =
-    match m.State with
-    | NotStarted -> failwith "Match not started"
-    | FirstInnings -> readPlayer label m.SecondInnings.BattingTeam
-    | SecondInnings -> readPlayer label m.FirstInnings.BattingTeam
-    | Completed -> failwith "Match completed"
-
-let readBatsmanDetail = readBatsman >> newBatsmanDetail
-
-let getBowlingTeam innings =
-    match innings.BattingTeam with
-    | TeamA -> TeamB
-    | TeamB -> TeamA
-    | NotSet -> NotSet
-
 let newBowlerDetail (player: Player) : BowlerDetail =
     { Bowler = player; Runs = 0; Maidens = 0; Wickets = 0; Extras = 0 }
-     
-let initMatch() =
 
+/////////////////////////////////////////////////
+// Functions that return Match
+/////////////////////////////////////////////////
+
+let newMatch() : MatchBeforeStart =
+    // Functions
     let readPlayers teamSize =
         let label i = "Player " + string i
         List.map (label >> readLine) [1..teamSize]
@@ -257,19 +204,29 @@ let initMatch() =
     let readTeam label teamSize : Team =
         { Name = readLine label; Players = readPlayers teamSize }
 
-    let firstBatsman() = readBatsmanDetail "Facing Batsman" (NotOut(Facing))
-    let secondBatsman() = readBatsmanDetail "Other Batsman" (NotOut(NotFacing))
-    let firstBowler() = getBowlingTeam m.FirstInnings |> readPlayer "Bowler" |> newBowlerDetail
+    // Start getting input
+    let teamSize = readInt "Team Size"
+    let teamA = readTeam "Team A" teamSize
+    let teamB = readTeam "Team B" teamSize
+    let overs = readInt "Overs" |> uint8
+    { Overs = overs; Teams = (teamA, teamB) }
 
-    m <- { m with TeamSize = readInt "Team Size" }
-    m <- { m with TeamA = readTeam "Team A" m.TeamSize }
-    m <- { m with TeamB = readTeam "Team B" m.TeamSize }
-    m <- { m with Overs = readInt "Overs" }
-    m <- { m with State = FirstInnings }
-    m <- { m with FirstInnings = { m.FirstInnings with BattingTeam = readTossOutcome()
-                                                       Batting = firstBatsman() :: secondBatsman() :: m.FirstInnings.Batting
-                                                       Bowling = firstBowler() :: m.FirstInnings.Bowling } }
-    m <- { m with SecondInnings = { m.SecondInnings with BattingTeam = getBowlingTeam m.FirstInnings } }
+let newMatchFirstInnings (x: MatchBeforeStart) : MatchOnFirstInnings
+    printfn fst(x.Teams).Name
+
+/////////////////////////////////////////////////
+// Functions that mutate m
+/////////////////////////////////////////////////
+     
+let initMatch () =
+    match m with
+    | None -> m <- BeforeStart(newMatch())
+    | _ -> printfn "Already a match exists!"
+
+let startMatch () =
+    match m with
+    | BeforeStart(x) -> m <- FirstInnings(newMatchFirstInnings x)
+    | _ -> printfn "Match already started!"
 
 /////////////////////////////////////////////////
 // All the Print functions below this
@@ -316,7 +273,7 @@ let processCommand cmd =
     | ScoreCommand(scoreCommand) -> () //processScoreCommand scoreCommand
     | PrintScore -> printScore()
     | InvalidCommand -> printfn "Invalid Command\n"
-    | Test -> printf "%A" <| readOptions "Which team you support?" [TeamA; TeamB]
+    | Test -> readOptions "Which team you support?" [TeamA; TeamB] |> ignore
 
 /////////////////////////////////////////////////
 // Entry Point
